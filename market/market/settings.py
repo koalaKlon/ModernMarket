cd0a7344de9b525f9f3d1977153e4bdd4d91d150
KoalaKlon <koala.klon@gmail.com>
2023-07-22 16:58:33 +0300

    Finally got the right Django Project structure

--- market/market/settings.py
+++ market/market/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'store', 'templates', 'store')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
