af662115fbfde418c44a83ef3d1229ae02fba272
KoalaKlon <koala.klon@gmail.com>
2023-08-26 18:36:12 +0300

    RabbitMQ + Celery

--- market/market/settings.py
+++ market/market/settings.py
@@ -5,8 +5,6 @@ from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SECRET_KEY'
