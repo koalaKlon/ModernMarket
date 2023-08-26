dc3d69d05fa268238a4717c53770052db8c63ad9
KoalaKlon <koala.klon@gmail.com>
2023-08-26 18:35:25 +0300

    RabbitMQ + Celery

--- market/market/settings.py
+++ market/market/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0d%&(donx1tlwl49tpi%&z_w1k-#evb8o0qe^@#_kg3v#@q4=#'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
